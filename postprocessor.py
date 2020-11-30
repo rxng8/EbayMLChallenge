bb543a03ea2440e30d4034e32ec533780d0b30f2
Alex Nguyen <alexvn.edu@gmail.com>
2020-11-29 19:00:56 -0500

    create template for after clustering

---
+++ postprocessor.py
@@ -0,0 +1,13 @@
+
+from birch import BirchTree
+
+
+class PostProcessor:
+    def __init__(self, bt: BirchTree):
+        self.bt = bt
+
+    def process(self):
+        pass
+
+    def export_tsv(self):
+        pass
